05258399d9ae34df5d56db85ac00d39cf6990108
Ripa <davide.ripanucci@gmail.com>
2024-12-01 18:46:14 +0100

    commit

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ IMPORTANT: Do not care about the user location, we will provide this information
 
 	if selected_call.function.name == "get_cheapest_list_of_products":
 		messages.append({"role": "user", "content": "Make a balanced choice between the cheapest products and the distance to the store and suggest to the user a unique store where to buy the products."})
-
+	selected_call = None
 	response = client.chat.completions.create(
 		model=MODEL,
 		messages=messages,
